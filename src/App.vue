b2eb399cdcdd367bf55c242a2d3c46bf94a02a53
danainan <danainan555@gmail.com>
2023-09-21 15:12:12 +0700

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/logo.svg 	new file:   src/assets/style.css 	new file:   src/components/navbar.vue 	new file:   src/firebase/firebase.js 	new file:   src/main.js 	new file:   src/router/index.js 	new file:   src/views/Login.vue 	new file:   tailwind.config.js 	new file:   vite.config.js

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import navbar from './components/navbar.vue';
+</script>
+<template>
+  <navbar></navbar>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-active btn-primary">Primary</button>
+  </div>
+</template>
+
+
+
---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgIiKXaugbLIX_Zr6Gc9tWKci2d3XUxKE",
+  authDomain: "databaseforvuejs.firebaseapp.com",
+  projectId: "databaseforvuejs",
+  storageBucket: "databaseforvuejs.appspot.com",
+  messagingSenderId: "476570979952",
+  appId: "1:476570979952:web:1702f1a21b80aa6597eeb5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
